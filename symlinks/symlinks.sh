6111aa81949fd52962a8544210103dd4ce59898f
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2021-05-08 13:01:20 +0100

    🚚 add symlinks

---
+++ symlinks/symlinks.sh
@@ -0,0 +1,6 @@
+#!usr/bin/env bash
+
+ln -s .dotfiles/.gitignore_global ~/.gitignore_global
+ln -s .dotfiles/.gitconfig ~/.gitconfig
+ln -s .dotfiles/.bashrc ~/.bashrc
+ln -s .dotfiles/.zshrc ~/.zshrc
