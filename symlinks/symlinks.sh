a54c64cce2c303552b85982703a65e9591e15cdf
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2021-05-08 14:40:12 +0100

    🚚 add new dirs and update symlinks

--- symlinks/symlinks.sh
+++ symlinks/symlinks.sh
@@ -1,6 +1,8 @@
 #!usr/bin/env bash
 
-ln -s .dotfiles/.gitignore_global ~/.gitignore_global
-ln -s .dotfiles/.gitconfig ~/.gitconfig
-ln -s .dotfiles/.bashrc ~/.bashrc
-ln -s .dotfiles/.zshrc ~/.zshrc
+ln -s .dotfiles/git/.gitignore_global ~/.gitignore_global
+ln -s .dotfiles/git/.gitconfig ~/.gitconfig
+ln -s .dotfiles/shell/bash/.bashrc ~/.bashrc
+ln -s .dotfiles/shell/bash/.profile ~/.profile
+ln -s .dotfiles/shell/zsh/.zshrc ~/.zshrc
+ln -s .dotfiles/shell/zsh/.zshenv ~/.zshenv
