63eba410db3c594982b45388e089f83de84124f6
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2021-09-28 20:03:13 +0100

    add docker logs container script

--- symlinks/symlinks.sh
+++ symlinks/symlinks.sh
@@ -2,6 +2,7 @@
 
 ln -s .dotfiles/git/.gitignore_global ~/.gitignore_global
 ln -s .dotfiles/git/.gitconfig ~/.gitconfig
+ln -s .dotfiles/git/.gitconfig-leanmind ~/.gitconfig-leanmind
 ln -s .dotfiles/shell/bash/.bashrc ~/.bashrc
 ln -s .dotfiles/shell/bash/.profile ~/.profile
 ln -s .dotfiles/shell/zsh/.zshrc ~/.zshrc
