a62b513f3bc1f4613476fc22f828d6dd7b18844c
Manuel Pérez Regalado <perezregaladomanuel@gmail.com>
2021-12-22 22:42:24 +0100

    add prepare system notes

--- prepare-system.sh
+++ prepare-system.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 sudo apt update && sudo apt install zsh git curl fzf -y
+#oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+#oh-my-zsh theme
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+#set zsh as default prompt
 chsh -s $(which zsh)
